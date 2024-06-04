807f2ffd7fabb665bf79a1fd0c21b62d1a87149d
prashant18108 <singhprashant18108@gmail.com>
2024-06-04 15:40:57 +0530

    blogs app, blog model, media files configuration

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files configuration
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
---
+++ blogs/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.category_name
+    
+
+STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+)
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=2000)
+    status = models.CharField(max_length=20 , choices=STATUS_CHOICES, default='draft')
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created_at = timezone.now()
+        self.updated_at = timezone.now()
+        return super(Blog, self).save(*args, **kwargs)    
+
+    class Meta:
+
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        return self.title
+
