b4ef994fe7acc2a6579af8f10ffa3e088ec93e93
prashant18108 <singhprashant18108@gmail.com>
2024-06-12 01:29:29 +0530

    manage Users functionality for managers

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm, BlogPostForm
+from .forms import CategoryForm, BlogPostForm, AddUserForm, EditUserForm
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User, Permission
 
 
 @login_required(login_url='login')
@@ -129,3 +130,58 @@ def delete_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect('posts')
+
+
+def users(request):
+    users = User.objects.all()
+    permissions = get_user_permissions(request.user)
+    print(permissions)
+    context = {
+        'users':users
+    }
+    return render(request, 'dashboard/users.html', context)
+
+
+def add_user(request):
+    form = AddUserForm()
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    context = {
+        'form':form
+    }
+    return render(request, 'dashboard/add_user.html', context)
+
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    form = EditUserForm(instance=user)
+    if request.method =='POST':
+        form = EditUserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    context = {
+        'form':form,
+        'user':user
+    }
+    return render(request, 'dashboard/edit_user.html', context)
+
+
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
+
+
+# Method to view permissions
+def get_user_permissions(user):
+    if user.is_superuser:
+        return Permission.objects.all()
+    return user.user_permissions.all() | Permission.objects.filter(group__user=user)
